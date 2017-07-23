8bea31e2e8b3cbd95a9e58dfaabae03183a936ac
Douglas Costa <dwcosta@users.noreply.github.com>
2017-07-23 16:06:42 -0300

    fibonacci sequence

---
+++ fibonacci.c
@@ -0,0 +1,40 @@
+//fibonacci sequence list
+//code by douglas costa - dwcosta@gmail.com - 22/07/2017
+//it generates a vector using dinamic allocation for how many numbers you wanna list.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n,i;
+    printf("\nenter how many numbers you wanna list: ");
+    scanf("%d",&n);
+    if (n<=0){
+        printf("\nenter a positive number\n\n");
+        exit(1);
+    }
+    if (n==1){
+        printf("\nsequence: 0\n\n");
+        exit(1);
+    }
+    if (n>93){
+        printf("\ntoo high to list. max 93\n\n");
+        exit(1);
+    }
+    if (n > 2){
+        long int *p = malloc(n*sizeof(int));
+        if(p == NULL){
+            printf("\nERROR - insuficient memory!\n");
+            exit(1);
+        }
+        p[0] = 0;
+        p[1] = 1;
+        for(i = 2 ; i < n ; i++) p[i] = p[i-1] + p[i-2];
+        printf("\n\n");
+        for(i = 0 ; i < n ; i++) printf("%ld\n",p[i]);
+        printf("\n\n");
+        
+    }
+    
+}
